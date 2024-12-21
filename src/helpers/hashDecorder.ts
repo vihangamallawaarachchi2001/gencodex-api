9ddc03113e6d3ddaed39550e51814ccf425c3aa3
Vihanga Mallawaarachchi <vihanganethusara00@gmail.com>
2024-12-21 22:26:37 +0530

    rebase

--- src/helpers/hashDecorder.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt';
-import { user } from '../types/user.type';
-
-const hashDecorder = async (userPassword: user['password'], dbPassword: user['password']): Promise<boolean> => {
-    return await bcrypt.compare(userPassword, dbPassword);
-    
-}
-
-export default hashDecorder;
